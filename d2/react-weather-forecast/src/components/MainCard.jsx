84b5acc1e6acf1a9565e9061223ab87087fbcae4
Tochinskyi Illya <naehk532@gmail.com>
2025-10-30 00:29:54 +0200

    little progress

---
+++ d2/react-weather-forecast/src/components/MainCard.jsx
@@ -0,0 +1,15 @@
+export function MainCard() {
+  return (
+    <>
+      {/* обертка */}
+      <div className="">
+        {/* контейнер */}
+        <div className="">
+          <h2>
+            Today <img src="" alt="" />
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
